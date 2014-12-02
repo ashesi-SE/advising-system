f995d05677f8993c169fd90d4b6f86fb8332f088
Abdul Muhsin <abdul.iddris13@gmail.com>
2014-12-02 01:05:33 +0000

    changes to advisor class

--- models/advisor_class.php
+++ models/advisor_class.php
@@ -31,7 +31,7 @@ class advisor_class extends adb {
         
     }
     function insert_notes_per_session($student_id){
-   $query = "Inser into ";
+   $query = "Inser into  ";
 //                            print $query;  
         return $this->query($query);  
         
@@ -42,4 +42,35 @@ class advisor_class extends adb {
         return $this->query($query);  
         
     }
+    function loginAsAdvisor($user,$pass){
+       $query = "select count(*) as c from faculty where username = '$user' and password = '$pass'";
+       $this->query($query);
+       
+        $result = $this->fetch();
+      if ($result['c'] == 1) {
+         return true;
+      } else {
+         return false;
+      }
+       
+       
+   }
+   
+   function loadUserProfile($username) {
+      //load username and other informaiton into the session      
+      $query = "select * from faculty where username = '$username';";
+
+      $this->query($query);
+
+      $result = $this->fetch();
+      session_start();
+
+      $_SESSION['username'] = $username;
+//      $_SESSION['role'] = $result['role_role_id'];
+      $_SESSION['id'] = $result['faculty_id'];
+//      $_SESSION['team_id'] = $result['team_team_id'];
+
+
+      return $result;
+   }
 }
